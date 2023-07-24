e7e31fcd68f3a33b83fd90d9193d51e62f3c202c
Olatbosun <boyxdik@gmail.com>
2023-07-24 07:14:28 -0700

    keygen update

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-#define PASSWORD_LENGTH 13
-
+/**
+ * main - generates keygen.
+ * Return: 0 Always.
+ */:wq
 int main(void)
 {
-    char password[PASSWORD_LENGTH];
-    int i, sum = 0, diff;
+	int r = 0, c = 0;
+	time_t t;
 
-    srand(time(0)); // Seed the random number generator with the current time
-
-    // Generate random characters for the password
-    for (i = 0; i < PASSWORD_LENGTH - 1; i++)
-    {
-        password[i] = rand() % 94 + 33; // Generate a random character between ASCII 33 ('!') and ASCII 126 ('~')
-        sum += password[i];
-    }
-
-    diff = 2772 - sum;
-    password[PASSWORD_LENGTH - 1] = diff;
-
-    printf("%s\n", password);
-
-    return 0;
+	srand((unsigned int) time(&t));
+	while (c < 2772)
+	{
+		r = rand() % 128;
+		if ((c + r) > 2772)
+			break;
+		c = c + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - c));
+	return (0);
 }
