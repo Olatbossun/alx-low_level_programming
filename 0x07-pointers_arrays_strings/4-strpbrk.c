eaa94feb80c908086bbcd15f3b6dd64e54817301
Olatbossun <boyxdik@gmail.com>
2023-08-01 04:01:02 +0100

    correction

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -9,15 +9,17 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	unsigned int i, j;
+    int j;
 
-	for (i = 0; *(s + i) != '\0'; i++)
-	{
-		for (j = 0; *(accept + j) != '\0'; j++)
-		{
-			if (*(s + i) == *(accept + j))
-				return (s + i);
-		}
-	}
-	return ('\0');
+    for(; *s; s++)
+    {
+        for (j = 0; accept[j]; j++)
+        {
+            if (*s == accept[j])
+            {
+                return (s);
+            }
+        }
+    }
+    return ('\0');
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,14 +9,17 @@
  */
 void print_diagsums(int *a, int size)
 {
-	int i, sum1 = 0, sum2 = 0;
-
-	for (i = 0; i < (size * size); i++)
+	int index, sum1 = 0, sum2 = 0;
+	for (index = 0; index < size; index++)
 	{
-		if (i % (size + 1) == 0)
-			sum1 += *(a + i);
-		if (i % (size - 1) == 0 && i != 0 && i < size * size - 1)
-			sum2 += *(a + i);
+		sum1 += a[index];
+		a += size;
+	}
+	a -= size;
+	for (index = 0; index < size; index++)
+	{
+		sum2 += a[index];
+		a -= size;
 	}
 	printf("%d, %d\n", sum1, sum2);
 }
