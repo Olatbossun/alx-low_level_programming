7c96ef596148a9165fe9c7c6f5650e6e48afc1f3
Olatbossun <boyxdik@gmail.com>
2023-08-01 07:35:58 +0100

    corrections

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,25 +1,21 @@
 #include "main.h"
-
 /**
- * _strpbrk - searches a string for any of a set of bytes.
- * @s: first string.
- * @accept: second string.
- * Return: a pointer to the byte in s that matches one of the
- * bytes in accept, or NULL if no such byte is found.
- */
+* _strpbrk - Entry point
+* @s: input
+* @accept: input
+* Return: Always 0 (Success)
+*/
 char *_strpbrk(char *s, char *accept)
 {
-    int j;
-
-    for(; *s; s++)
-    {
-        for (j = 0; accept[j]; j++)
-        {
-            if (*s == accept[j])
-            {
-                return (s);
-            }
-        }
-    }
-    return ('\0');
+int k;
+while (*s)
+{
+for (k = 0; accept[k]; k++)
+{
+if (*s == accept[k])
+return (s);
+}
+s++;
+}
+return ('\0');
 }
--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -1,25 +1,23 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_diagsums - prints the sum of the two
- * diagonals of a square matrix of integers
- * @a: input pointer.
- * @size: size of the matrix
- * Return: no return.
- */
+* print_diagsums - Entry point
+* @a: input
+* @size: input
+* Return: Always 0 (Success)
+*/
 void print_diagsums(int *a, int size)
 {
-	int index, sum1 = 0, sum2 = 0;
-	for (index = 0; index < size; index++)
-	{
-		sum1 += a[index];
-		a += size;
-	}
-	a -= size;
-	for (index = 0; index < size; index++)
-	{
-		sum2 += a[index];
-		a -= size;
-	}
-	printf("%d, %d\n", sum1, sum2);
+int sum1, sum2, y;
+sum1 = 0;
+sum2 = 0;
+for (y = 0; y < size; y++)
+{
+sum1 = sum1 + a[y * size + y];
+}
+for (y = size - 1; y >= 0; y--)
+{
+sum2 += a[y * size + (size - y - 1)];
+}
+printf("%d, %d\n", sum1, sum2);
 }
